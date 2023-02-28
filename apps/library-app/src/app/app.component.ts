a18b8b15b97b6569274ada00c58811c4533d3e42
John Bluis <john.bluis@jax.org>
2023-02-28 10:35:38 -0500

    Initial commit

---
+++ apps/library-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-test-library-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'library-app';
+}
